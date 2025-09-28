ffb1348ec9e27bed365aac47adf6648f17297080
vikornienko <vikornienko76@gmail.com>
2025-09-28 14:44:06 +0500

    Was adding index.html file and change code in main.js file for show window on the screen.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello from Electron js!");
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 600,
+        height: 400
+    });
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+})
