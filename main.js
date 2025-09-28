14a912bf465df37c3fc2d63f839e18ae97f7227f
vikornienko <vikornienko76@gmail.com>
2025-09-28 20:02:28 +0500

    Add system information on window app.

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('node:path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 600,
-        height: 400
+        height: 400,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     });
     win.loadFile('index.html');
 }
---
+++ renderer.js
@@ -0,0 +1,6 @@
+﻿const information = document.getElementById('info');
+
+information.innerText = `В этом приложении исопльзуются \n
+версия Хром: v. ${versions.chrome()}, \n
+версия node js: v. ${versions.node()}, \n 
+версия electron js: v. ${versions.electron()}.`;
